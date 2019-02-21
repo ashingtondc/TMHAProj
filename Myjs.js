0184a6b91ab498ca128f2dd183b295d61f4b5cdd
Ashok Parasa <aparasa@calpoly.edu>
2019-02-21 00:25:58 -0800

    Core functionality working

--- Myjs.js
+++ Myjs.js
@@ -1,7 +1,7 @@
 /**
  * 
  */
-src='https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.5.1/tabletop.min.js'
+src='./tabletop.js'
 var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1U-j5z1X88miWur9b2I-RkD5W9zl1_7SVTND93ToX6Yo/edit?usp=sharing';
 let region;
 let city;
@@ -9,6 +9,7 @@ let care;
 
 function process() 
 {
+	//window.location.href = './display.html';
 	region = document.getElementById("region").value;
 	city = document.getElementById("city").value;
 	care = document.getElementById("care").value;
@@ -25,19 +26,33 @@ function init() {
 }
 
 function showInfo(data, tabletop) {
-  alert('Successfully processed!')
+  //alert('Successfully processed!')
   let sheet = tabletop.sheets(region);
   let stuff = sheet.all();
-  console.log(sheet.prettyColumns);
+  let headers = Object.keys(sheet.prettyColumns);
+  let table = document.createElement('table');
+  let header = table.createTHead();
+  let headerRow = header.insertRow(0);
+  let i;
+  for (i = 0; i < headers.length; i++)
+  {
+	  let cell = headerRow.insertCell(i);
+	  cell.innerHTML = "<b>" + headers[i].toUpperCase() + "</b>";
+  }
   stuff.forEach(function(house)
   {
 	  if (house.Location.includes(city) && house.Type_of_Care.includes(care))
 	  {
-		  console.log(house);
-		  let tr;
-		  tr += ("<td>" + house.Location + "</td>");
+		  let values = Object.values(house);
+		  let newRow = table.insertRow();
+		  for (let val of values)
+		  {
+			  let newCell = newRow.insertCell();
+			  newCell.innerHTML = val;
+		  }
 	  }
   })
+  document.body.appendChild(table);
   //console.log(stuff[0].Location);
   //console.log(data);
 }
